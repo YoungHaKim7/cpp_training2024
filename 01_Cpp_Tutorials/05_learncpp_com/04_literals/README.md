dac4542cae885bc9c9b2e8bbf4722f81f2ba5993
YoungHa Kim <ytok1108@aol.com>
2023-11-07 00:53:05 +0900

    Makefile C++

--- 01_Cpp_Tutorials/05_learncpp_com/04_literals/README.md
+++ 01_Cpp_Tutorials/05_learncpp_com/04_literals/README.md
@@ -1,5 +1,9 @@
 # Result
 
 ```
+$ g++ -std=c++2b -Wall -Wextra -pedantic -pthread -pedantic-errors ./src/main.cpp -lm && ./a.out
+
+5
+5
 
 ```
--- 01_Cpp_Tutorials/05_learncpp_com/04_literals/src/main.cpp
+++ 01_Cpp_Tutorials/05_learncpp_com/04_literals/src/main.cpp
@@ -3,7 +3,8 @@
 int main() {
 	std::cout << 5 << '\n'; // print the value of a literal
 
-	int x = { 5 };
+	// int x = { 5 };
+	int x{5};
 	std::cout << x << '\n'; // print the value of a variable
 	return 0;
 }
