5b680ddab4665024363d195c71c490a6f5f391ee
YoungHa Kim <ytok1108@aol.com>
2023-11-26 22:33:15 +0900

    Statement

---
+++ 01_Cpp_Tutorials/09_31hr_Modern_cpp_training_freeCodeCamp_org/005_statement/README.md
@@ -0,0 +1,12 @@
+# Result
+
+```
+
+$ g++ -c -std=c++2b -pedantic -pthread -pedantic-errors -Wall -Wextra -ggdb ./src/main.cpp
+mv *.o ./target/
+$ g++ ./target/*.o -o ./target/main
+./target/main
+
+First number : 3
+Second number : 7
+```
---
+++ 01_Cpp_Tutorials/09_31hr_Modern_cpp_training_freeCodeCamp_org/005_statement/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main(int argc, char **argv) {
+	int firstNumber = {3}; // Statement
+	int secondNumber = {7};
+
+
+	std::cout << "First number : " << firstNumber << '\n';
+	std::cout << "Second number : " << secondNumber << '\n';
+
+	return 0;
+}
