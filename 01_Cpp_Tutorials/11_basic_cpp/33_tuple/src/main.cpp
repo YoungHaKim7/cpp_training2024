d19dc82a4f11c20c98c1477d082e430b1e006db9
GYoung <ytok1108@aol.com>
2025-09-14 19:05:57 +0900

    - https://en.cppreference.com/w/cpp/utility/tuple.html

--- 01_Cpp_Tutorials/11_basic_cpp/33_tuple/src/main.cpp
+++ 01_Cpp_Tutorials/11_basic_cpp/33_tuple/src/main.cpp
@@ -4,12 +4,12 @@
 // Function returning RGB color as a tuple
 std::tuple<int, int, int> getColor()
 {
-    return {255, 128, 64}; // example RGB values
+    return { 255, 128, 64 }; // example RGB values
 }
 
 int main()
 {
-    auto [r, g, b] = getColor();  // structured binding
+    auto [r, g, b] = getColor(); // structured binding
 
     std::cout << "color: " << r << ", " << g << ", " << b << "\n";
 
